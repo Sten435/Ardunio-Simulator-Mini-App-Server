a30509725477c97363212dae1a992432e94052e5
Sten435 <stan@anticheatpd.com>
2020-12-11 23:08:23 +0100

    Stable

--- README.md
+++
@@ -1,3 +0,0 @@
-# Ardunio-Simulator-Mini-App-Server
-
-Start the app with: NPM START
--- server.js
+++ server.js
@@ -1,4 +1,8 @@
-var server = require('http').createServer();  
+var server = require('http').createServer();
+const usbDetect = require('usb-detection');
+const serialIo = require('serial-io');
+const SerialPort = require('serialport');
+const Readline = SerialPort.parsers.Readline
 var io = require('socket.io')(server, {
     cors: {
         methods: ["GET", "POST"],
@@ -8,9 +12,55 @@ var io = require('socket.io')(server, {
 });
 
 io.on('connection', function(client) {
-  client.on('msg', function(data) {
-      console.log(data);
+  usbDetect.startMonitoring();
+
+  usbDetect.on('add', function (){
+      setTimeout(() => {  
+        serialIo.ports().then(path=>{
+          client.emit('device_added', path)
+        })
+      }, 500);
+  })
+ 
+  usbDetect.on('remove', function (){
+      setTimeout(() => {  
+        serialIo.ports().then(path=>{
+          client.emit('device_removed', path)
+        })
+    }, 500);
   });
+
+  client.on('portData_bautData', ((data)=>{
+    let port = data.PORTValue
+    let bautrate = data.BautrateValue
+    const parser = new Readline()
+    const serialport = new SerialPort(port, { 
+      baudRate: parseInt(bautrate),
+      lock: false
+    })
+    serialport.on('open', function(){
+      
+      serialport.pipe(parser)
+        parser.on('data', ((data)=>{
+          client.emit('mainDataFromLocalServer', data)
+        }))
+        client.on('disconnectPort', function(){
+          serialport.destroy()
+        })
+
+        serialport.on('error', function(err){
+          console.log(err);
+        })
+    })
+  }))
+
+  client.on('Request_ports', function(){
+    setTimeout(() => {  
+      serialIo.ports().then(path=>{
+        client.emit('sendPort', path)
+      })
+    }, 500);
+  })
 });
 
 server.listen(5000);
